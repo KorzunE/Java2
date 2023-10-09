4a1db9522d3284790949203e5a2126e73713e670
Evgenii Korzun <89513152520@yandex.ru>
2023-10-09 13:55:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 15_200;
+        int oneMiles = 20;
+        int bonusMiles = ticket / oneMiles;
+
+        System.out.println("Ваши бонусные мили " + bonusMiles);
+    }
+}
